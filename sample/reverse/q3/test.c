41e455f0c5a92950070d9ecc47f2fc48e3d5be78
Eudaemon <eudaemonal@gmail.com>
2016-11-01 17:04:03 +1100

    update

---
+++ sample/reverse/q3/test.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/*
+ Linked list possibly
+*/
+
+
+
+void sub_4012D0(int arg_0) {
+	var_14 = arg_0;
+	
+	var_18 = var_14 + 1;
+	
+	while(var_19[i]!= 0){
+		var_19[i] = var_14[i]
+		i++;
+	}
+	var_20 = var_14 - var_18;
+
+	Size = var_20;
+
+	Memory = malloc(Size);
+	memset(Memory, 0, Size);
+
+	var_8 = 0;
+	var_10 = 0;
+
+	do {
+		a = arg_0 + var_8;
+		if(a != 0x2E){
+			c = Memory + var_10
+			d = arg_0 + var8;
+			a = d;
+			Memory = arg_0
+			var_10 ++;
+		}
+	}while(Size==var_8)
+
+	var_24 = Memory;
+	var_28 = arg_0;
+	var_2C = var_28;
+
+	do{							//copying string var_24 to var_28
+		var_2D = var_24;
+		var_28 = var_2D;
+		
+		var_24 ++;
+		var_28 ++;
+	}while (var_2D != 0)
+	
+	arg_0 + var_10
+
+	free(Memory);
+
+}
+
+
+
+
+int main(int argc, char *argv[]){
+	
+}
---
+++ sample/reverse/q4/test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+void sub_80483F4(int arg_0) {
+	
+
+	int d = 0x4EC4EC4F;
+
+	d = d * arg_0;
+	d = d/4
+
+	arg_0 = arg_0/(2^0x1F);
+	a = arg_0;
+	
+
+	return (d-arg_0);
+
+}
+
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+}

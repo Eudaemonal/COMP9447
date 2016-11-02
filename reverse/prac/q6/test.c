7815bcec7660516599e84f9cddeb942e9faad169
Eudaemon <eudaemonal@gmail.com>
2016-11-02 14:12:11 +1100

    update

---
+++ reverse/prac/q6/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/*
+Create a new node at the end of the linked list
+*/
+
+
+struct List {
+	int elem;
+	struct List* next;
+}
+
+void sub_401020(List* arg_0, int arg_4) {
+	*new = malloc(8);
+	if(arg_0==0) return;
+
+	*curr = arg_0->offset4;
+	while(*curr!=0){
+		*curr = *curr->offset4
+	}
+
+	*curr->offset4 = *new
+	*new->offset4 = NULL;
+	*curr->offset0 = arg_4;
+
+
+}
